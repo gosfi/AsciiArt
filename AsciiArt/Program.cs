7589a9a36c8b9e0f0cf4af80d7b2588ea7e0dc49
Samuel Lefrançois <44752770+gosfi@users.noreply.github.com>
2022-09-12 09:07:14 -0400

    basic program done
    still TODO:
    
    1. get code for resizing picture before doing the Ascii Art
    
    2 . add option for different shading

--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -1,37 +1,78 @@
 ﻿using System;
+using System.Drawing;
 using ImageMagick;
 namespace AsciiArt
 {
     internal class Program
     {
-
+        static string asciiChars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
+        static int newWidth, newHeight;
         static void Main(string[] args)
         {
             MagickNET.Initialize();
-
-            using var image = new MagickImage("C:\\Users\\Sam\\Desktop\\ascii-pineapple.jpg");
-            Console.WriteLine("successfully constructed pixel matrix!");
-            Console.WriteLine($"Matrix size: {image.Width} x {image.Height}");
+            char[] reversed = asciiChars.ToCharArray();
+            Array.Reverse(reversed);
+            asciiChars = new string(reversed);
+            using var image = new MagickImage("C:\\Users\\Sam\\Desktop\\e3e5c94e50fa05ea03759292f3aea547.jpg");
+            newWidth = image.Width;
+            newHeight = image.Height;
             using IPixelCollection<ushort> pixels = image.GetPixels();
-            Color?[,] pixelMatrix = PixelMatrix(image.Width, image.Height, pixels);
-            Console.WriteLine("");
-            
+            Color[,] pixelMatrix = PixelMatrix(pixels);
+            byte[,] brightnessMatrix = new byte[newWidth, newHeight];
+            brightnessMatrix = GetBrightnessMatrix(pixelMatrix, brightnessMatrix);
+
+            PrintAsciiArt(brightnessMatrix);
         }
 
-        static Color?[,] PixelMatrix(int width, int height, IPixelCollection<ushort> pixels)
+        static Color[,] PixelMatrix(IPixelCollection<ushort> pixels)
         {
 
-            Color?[,] colors = new Color?[width, height];
-            for (int y = 0; y < height; y++)
+            Color[,] colors = new Color[newWidth, newHeight];
+            for (int x = 0; x < colors.GetLength(0); x++)
             {
-                for (int x = 0; x < width; x++)
+                for (int y = 0; y < colors.GetLength(1); y++)
                 {
                     var pixel = pixels.GetPixel(x, y).ToColor();
-                    colors[x, y] = new((byte)pixel.R, (byte)pixel.G, (byte)pixel.B);
+                    colors[x, y] = Color.FromArgb((byte)pixel.A, (byte)pixel.R, (byte)pixel.G, (byte)pixel.B);
                 }
             }
 
             return colors;
+        }
+
+        static byte SetBrightness(Color color)
+        {
+            return (byte)(0.21f * color.R + 0.72f * color.G + 0.07f * color.B);
+        }
+
+        static byte[,] GetBrightnessMatrix(Color[,] pixelMatrix, byte[,] brightMatrix)
+        {
+            for (int y = 0; y < newHeight; y++)
+            {
+                for (int x = 0; x < newWidth; x++)
+                {
+                    brightMatrix[x, y] = SetBrightness(pixelMatrix[x, y]);
+                }
+            }
+
+            return brightMatrix;
+        }
+
+        static void PrintAsciiArt(byte[,] brightnessMatrix)
+        {
+            for (int y = 0; y < newHeight; y++)
+            {
+                for (int x = 0; x < newWidth; x++)
+                {
+                    float charPos = MathF.Ceiling((brightnessMatrix[x, y] / 255f) * 64f);
+
+
+                    string asciiChar = new string(asciiChars[(byte)charPos], 3);
+                    Console.Write(asciiChar);
+
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
